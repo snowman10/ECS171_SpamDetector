223de907a7b64bf62abae02176804545f4008ee1
Brandon Hillig <69443656+bhillig@users.noreply.github.com>
2023-05-16 17:50:51 -0700

    Added beginning of logistic regression

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn
 import nltk
+import sklearn as sk
 
 from sklearn.model_selection import train_test_split 
 
@@ -10,8 +11,8 @@ data.drop(labels=['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1,inplace=True)
 data.rename(columns={'v1':'Class','v2':'Text'},inplace=True)
 data['numClass'] = data['Class'].map({'ham':0, 'spam':1})
 
-# nltk.download('stopwords')
-# stopwords = nltk.corpus.stopwords.words('english')
+nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('english')
 # print(stopwords)
 
 # print(data)
@@ -20,18 +21,18 @@ print(data)
 
 
 #importing the Stemming function from nltk library
-# from nltk.stem.porter import PorterStemmer
+from nltk.stem.porter import PorterStemmer
 # #defining the object for stemming
-# porter_stemmer = PorterStemmer()
+porter_stemmer = PorterStemmer()
  
 # #defining a function for stemming
-# def stemming(text):
-#   stem_text = [porter_stemmer.stem(word) for word in text]
-#   return stem_text
+def stemming(text):
+  stem_text = [porter_stemmer.stem(word) for word in text]
+  return stem_text
  
 # # applying function for stemming
-# data['Text']=data['Text'].apply(lambda x: stemming(x))
-# data['Text']=data['Text'].apply(lambda x: ''.join(x))
+  data['Text']=data['Text'].apply(lambda x: stemming(x))
+  data['Text']=data['Text'].apply(lambda x: ''.join(x))
 # print(data)
 
 # To do: STEMMING
@@ -40,5 +41,17 @@ print(data)
 X = data.drop('Class', axis = 1)
 y = data['Class']
 
-data_train, data_test, class_train, class_test = train_test_split(X, y, test_size=0.1)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, multilabel_confusion_matrix
+from sklearn.metrics import mean_squared_error, accuracy_score, precision_score, recall_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer("english")
+message_mat = vectorizer.fit_transform(X)
+
+data_train, data_test, class_train, class_test = train_test_split(message_mat, y, test_size=0.1, random_state=13)
+
+cls = LogisticRegression()
+cls.fit(data_train, class_train)
+# print(classification_report(class_test, cls.predict(data_test)))
 
