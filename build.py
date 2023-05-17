e9d286427701238d8ad486b99427b753037d72a6
snowman10 <kjafri10@gmail.com>
2023-05-16 18:59:24 -0700

    Update build.py

--- build.py
+++ build.py
@@ -2,9 +2,11 @@ from gui import Constructor
 import tkinter as tk
 
 window = Constructor(grid={"col":5,"row":5})
-window.BuildLabel(key="wow", text="Hello")
-window.setLabelGrid("wow", row=0, column=0)
-window.BuildEntry(key="predict")
-window.setEntryGrid("predict", row=1, column=1)
+window.BuildLabel(preset="title", key="wow", text="ECS 171 Final Project")
+window.setLabelGrid("wow", row=0, column=0, columnspan=5)
+window.BuildEntry(key="predict", width=20)
+window.setEntryGrid("predict", row=3, column=0, columnspan=5)
+window.BuildButton(preset="submit", key="submit")
+window.setButtonGrid(key="submit", column=0, row=4, columnspan=5)
 
 window.root.mainloop()
